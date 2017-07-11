533ac6418b709f396a975f2b071d31710098c07f
nekoambrosius <leo.eclipse.masamune@gmail.com>
2017-07-11 16:29:50 -0500

    scenario 4 cod

--- pyproject/scenario04/main.py
+++ pyproject/scenario04/main.py
@@ -2,7 +2,35 @@ import ccircle
 import connection
 
 con = connection.create()
-con.send('set_name', {'name': 'put_your_name_or_nickname_here'})
+con.send('set_name', {'name': 'JackAssless'})
+
+
+args = {
+    'vx': -43,
+    'vy': -24,
+}
+
+con.send('set_velocity', args)
+
+while True:
+    if ccircle.isKeyDown('left'):
+        args['vy'] = 0
+        args['vx'] = -50
+        con.send('set_velocity', args)
+    if ccircle.isKeyDown('right'):
+        args['vy'] = 0
+        args['vx'] = 50
+        con.send('set_velocity', args)
+    if ccircle.isKeyDown('up'):
+        args['vy'] = -50
+        args['vx'] = 0
+        con.send('set_velocity', args)
+    if ccircle.isKeyDown('down'):
+        args['vy'] = 50
+        args['vx'] = 0
+        con.send('set_velocity', args)
+
+
 
 # Write code to make money and kill the evil cat!
 # See readme.txt !
