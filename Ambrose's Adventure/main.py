355b382470b6174ca6d6917a1eb34fe8390c295d
nekoambrosius <leo.eclipse.masamune@gmail.com>
2017-07-14 10:17:44 -0500

    Ambrose's

--- Ambrose's Adventure/main.py
+++ Ambrose's Adventure/main.py
@@ -1,14 +1,30 @@
 import ccircle
-import rooms
-import Ambrose
 
-window = ccircle.Window("Ambroses Adventure", 2021, 2021)
-rooms = Ambrose.Rooms(place.Player)
+window = ccircle.Window("Ambroses Adventure")
+window.toggleMaximized()
 
-time = 0
 while window.isOpen():
     window.clear(0, 0, 0)
-    window.update()
-    room.update(dt)
-
-
+for i in range():
+        window.draw('AA_AmbroseSideStop.png')
+        window.draw('AA_AmbroseSideW.png')
+        window.draw('AA_AmbroseSideStop.png')
+        window.draw('AA_AmbroseSideW(2).png')
+while True:
+        if ccircle.isKeyDown('left'):
+            args['vy'] = 0
+            args['vx'] = -50
+            con.send('set_velocity', args)
+        if ccircle.isKeyDown('right'):
+            args['vy'] = 0
+            args['vx'] = 50
+            con.send('set_velocity', args)
+        if ccircle.isKeyDown('up'):
+            args['vy'] = -50
+            args['vx'] = 0
+            con.send('set_velocity', args)
+        if ccircle.isKeyDown('down'):
+            args['vy'] = 50
+            args['vx'] = 0
+            con.send('set_velocity', args)
+window.update()
--- Ambrose's Adventure/worlds.py
+++
@@ -1,8 +0,0 @@
-import ccircle
-class worlds:
-    def __init__(self, **kwargs):
-        self.imageBG = ccircle.Image('AA_Cabin(Inside).png')
-
-window = ccircle.Window("Cabin", 2021, 2021)
-while window.isOpen():
-        window.clear(0, 0, 0)
