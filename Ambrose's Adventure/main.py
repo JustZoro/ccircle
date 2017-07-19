c7d54d934f9b0e7f75891cbf2773b7f107a1d29a
NekoAmbrosius <leo.eclipse.masamune@gmail.com>
2017-07-19 16:19:54 -0500

    meh

--- Ambrose's Adventure/main.py
+++ Ambrose's Adventure/main.py
@@ -3,17 +3,13 @@ import world
 
 window = ccircle.Window("Ambroses Adventure")
 window.toggleMaximized()
-for i in range(2):
-    imageBG = ccircle.Image("image/AA_Cabin(Inside).png")
-    image = ccircle.Image("image/AA_AmbroseCharModel.png")
+imageBG = ccircle.Image("image/AA_Cabin(Inside).png")
 
-ambrose = world.Player(900, 244)
-
+ambrose = world.Ambrose(678, 109)
 while window.isOpen():
     window.clear(1, 1, 1)
     dt = 1/60
     imageBG.draw(2, 0, 1400, 730)
-    image.draw(900, 90, 89, 109)
 
     ambrose.update(dt)
     ambrose.draw()
--- Ambrose's Adventure/world.py
+++ Ambrose's Adventure/world.py
@@ -4,49 +4,60 @@ East = 1
 South = 2
 West = 3
 
-class Player:
+class Ambrose:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.tx = x
-        self.ty = y
-        self.image = [
-            ccircle.Image("image/AA_AmbroseCharModel.png"),
-            ccircle.Image("image/AA_AmbroseCharModel(2).png"),
-            ccircle.Image("image/AA_AmbroseCharModel(6).png"),
-            ccircle.Image("image/AA_AmbroseCharModel(3).png"),
-            ccircle.Image("image/AA_AmbroseCharModel(4).png"),
-            ccircle.Image("image/AA_AmbroseCharModel(5).png"),
-            ccircle.Image("image/AA_AmbroseSideStop.png"),
-            ccircle.Image("image/AA_AmbroseSideW.png"),
-            ccircle.Image("image/AA_AmbroseSideW(2).png"),
-            ccircle.Image("image/AA_AmbroseBehind.png"),
-            ccircle.Image("image/AA_AmbroseBehind(W).png"),
-            ccircle.Image("image/AA_AmbroseBehind(W2).png"),
-        ]
+        self.facing = "right"
+        self.action = "idle"
+        self.image = {
+            "walk_left":[ccircle.Image("image/AA_AmbroseSideW(2).png"),
+                            ccircle.Image("image/AA_AmbroseSideW.png"),
+                           ],
+            "walk_right":[ccircle.Image("image/AA_AmbroseCharModel(4).png"),
+                             ccircle.Image("image/AA_AmbroseCharModel(5).png"),
+                             ],
+            "walk_front":[ccircle.Image("image/AA_AmbroseCharModel(6).png"),
+                               ccircle.Image("image/AA_AmbroseCharModel(2).png"),
+                               ],
+            "walk_back":[ccircle.Image("image/AA_AmbroseBehind(W).png"),
+                                ccircle.Image("image/AA_AmbroseBehind(W2).png")
+                                ],
+            "idle_left":[ccircle.Image("image/AA_AmbroseSideStop.png"),
+                         ],
+            "idle_right":[ccircle.Image("image/AA_AmbroseCharModel(3).png"),
+                          ],
+            "idle_front":[ccircle.Image("image/AA_AmbroseCharModel.png"),
+                            ],
+            "idle_back":[ccircle.Image("image/AA_AmbroseBehind.png"),
+                             ]
+        }
 
-    if Player.isFacingE():
-        image.draw("")
+
 
     def draw(self):
-        self.image[self.facing].draw(900, 240, 89, 109)
+        state = self.action +"_"+ self.facing
+        self.image[state][0].draw(self.x, self.y, 89, 109)
 
-    def getName(self):
-        return 'pusheen'
+
 
     def update(self, dt):
         if ccircle.isKeyDown('left'):
-            self.y = 0
-            self.x = -50
-        if ccircle.isKeyDown('right'):
-            self.y = 0
-            self.x = 50
-        if ccircle.isKeyDown('up'):
-            self.y = -50
-            self.x = 0
-        if ccircle.isKeyDown('down'):
-            self.y = 50
-            self.x = 0
-
-
+            self.facing = "left"
+            self.action = "walk"
+            self.x  -=50*dt
+        elif ccircle.isKeyDown('right'):
+            self.facing = "right"
+            self.action = "walk"
+            self.x  +=50*dt
+        elif ccircle.isKeyDown('up'):
+            self.facing = "back"
+            self.action = "walk"
+            self.y -=50*dt
+        elif ccircle.isKeyDown('down'):
+            self.facing = "front"
+            self.action = "walk"
+            self.y  +=50*dt
+        else:
+            self.action = "idle"
 
